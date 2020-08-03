72fe0728a9cbbb567c1787cdab3ef53df859de23
Eri Schon <erischon@gmail.com>
2020-08-03 15:55:52 +0200

    intro to javascript 5

---
+++ fdcp-3-making_websites_interactive/fdcp-3b-intro-to-javascript/index_5.js
@@ -0,0 +1,53 @@
+// Javascript Objects Part 1
+
+// key: value
+var person = {
+    name: "Rick",
+    age: 70,
+    friends: ["Morty", "Joe", "Sam", "Samantha"],
+    address: {
+        street: "123 street",
+        city: "Somewhere"
+    }
+}
+
+
+// two notations for accessing an object's data
+
+// Dot notation "."
+
+// console.log(person.name)
+// console.log(person.age)
+// console.log(person.friends)
+
+// console.log(person.address.street)
+
+// Bracket notation []
+// console.log( person["name"] )
+
+// console.log( person["address"]["city"] )
+// console.log( person["address"].city )
+
+//////////////
+// EXERCISE //
+
+// Find 2 real world things you can describe in the form of an Object.  
+// Give those objects 1 of each data type (string, number, boolean, array, object)
+// Practice console.log() items in those objects using both dot "." and bracket "[]" notation.
+
+// Object 1:
+
+var animal = {
+    race: "felin",
+    color: "black",
+    age: 15,
+}
+
+// Object 2:
+
+var vehicule = {
+    type: "voiture",
+    brand: "mercedes",
+    km: 15000,
+}
+
