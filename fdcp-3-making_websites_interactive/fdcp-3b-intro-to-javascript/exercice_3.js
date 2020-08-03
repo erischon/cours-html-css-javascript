507b8810a63466cd95e2c944aa0c62f70d4271ac
Eri Schon <erischon@gmail.com>
2020-08-03 11:15:46 +0200

    Intro to javascript

---
+++ fdcp-3-making_websites_interactive/fdcp-3b-intro-to-javascript/exercice_3.js
@@ -0,0 +1,12 @@
+var namesArray = ["Steve", "John", "Betty", "Morty", "Rick", "Alan", "Sarah"]
+
+// 1. How would you console.log() the name Betty?
+
+// 2. How would you console.log() the name Sarah?
+
+// 3. What is the index of "Steve"?
+
+// EXTRA CREDIT
+// 4. Console.log() sarah using the namesArray while using the array.length property.
+
+ 
---
+++ fdcp-3-making_websites_interactive/fdcp-3b-intro-to-javascript/notes_3.md
@@ -0,0 +1,5 @@
+// Arrays are known as lists and contain items of a similar type.
+
+// Arrays are indexed starting at the number 0
+
+// Arrays have a pre-built property called length that will tell you the amount of items in the array.
