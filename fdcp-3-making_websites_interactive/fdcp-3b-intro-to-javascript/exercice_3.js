fb240030597d124b7ce7721fdec18319556e5263
Eri Schon <erischon@gmail.com>
2020-08-03 15:23:14 +0200

    Intro to javascript 4

--- fdcp-3-making_websites_interactive/fdcp-3b-intro-to-javascript/exercice_3.js
+++ fdcp-3-making_websites_interactive/fdcp-3b-intro-to-javascript/exercice_3.js
@@ -1,18 +1,13 @@
+// **** CHallenge Arrays ****
+
 var namesArray = ["Steve", "John", "Betty", "Morty", "Rick", "Alan", "Sarah"]
 
 // 1. How would you console.log() the name Betty?
-
-console.log(namesArray[2])
-
+console.log( namesArray[2] );
 // 2. How would you console.log() the name Sarah?
-
-console.log(namesArray[6])
-
+console.log( namesArray[6] );
 // 3. What is the index of "Steve"?
-
-0
-
+console.log( namesArray[0] );
 // EXTRA CREDIT
 // 4. Console.log() sarah using the namesArray while using the array.length property.
-
- 
+console.log( namesArray[ namesArray.length - 1 ]);
