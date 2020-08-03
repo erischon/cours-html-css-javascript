d84207d317d26dc709841bba2859803ace61f9fe
Eri Schon <erischon@gmail.com>
2020-08-03 15:03:26 +0200

    intro to javascript 3

--- fdcp-3-making_websites_interactive/fdcp-3b-intro-to-javascript/index_3_array.js
+++ fdcp-3-making_websites_interactive/fdcp-3b-intro-to-javascript/index_3_array.js
@@ -1,1 +1,14 @@
 // Javascript Arrays
+var myNumbers = [1, 2, 3, 4, 5]
+
+        // .   0 .      1 .      2 .     3 .       4
+var colors = ["blue", "green", "red", "yellow", "purple"]
+
+// console.log(colors[5])
+
+// Length
+// console.log( colors.length )
+
+// String
+var name = "joe"
+console.log( name.length )
