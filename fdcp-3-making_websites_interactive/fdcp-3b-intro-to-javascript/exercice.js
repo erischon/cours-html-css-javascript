6b5b5c7b0ef0b767509e0455a6e2a85bb5d48249
Eri Schon <erischon@gmail.com>
2020-08-03 11:07:34 +0200

    intro to javascript 1

---
+++ fdcp-3-making_websites_interactive/fdcp-3b-intro-to-javascript/exercice.js
@@ -0,0 +1,13 @@
+var name = "Toto"
+
+var age = 18
+
+var friends = [Titi, Tutu, Tata]
+
+var isThirsty = true
+
+var address = {
+    street: "truc machin",
+    streetNumber: 45,
+    town: "Paris"
+}
---
+++ fdcp-3-making_websites_interactive/fdcp-3b-intro-to-javascript/index_data-types.js
@@ -0,0 +1,37 @@
+// String
+// Number
+// Boolean
+// Array
+// Object
+
+// Primitives
+    // Strings = "hello"
+    // Numbers = 2
+    // Booleans
+    
+// Complex
+    // Array   = [1, 2, 3, 4]
+    // Objects
+    
+// String
+var name = 'Joe'
+    
+// Number
+    var age = 20
+    
+// Boolean = true or false
+    var isAlive = true
+    var hasDoneTaxes = false
+    
+// Array - List of similar items . (0 indexed)
+    var numbers = [1, 2, 3, 4, 5, 6, 7]
+    numbers[3]
+    
+// Object - Explains a singular item in depth . (key value pairs)
+    var person = {
+        name: "Steve",
+        age: 21,
+        hairColor: "brown",
+        isAlive: true
+    }
+    
