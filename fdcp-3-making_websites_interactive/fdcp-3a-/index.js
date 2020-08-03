577b5acd3bb5ac75c4fae68c7351792217f76102
Eri Schon <erischon@gmail.com>
2020-08-03 10:25:41 +0200

    Your first interactive website 7

--- fdcp-3-making_websites_interactive/fdcp-3a-/index.js
+++ fdcp-3-making_websites_interactive/fdcp-3a-/index.js
@@ -1,6 +1,9 @@
 var myButton = document.getElementById("btn")
+var myTitle = document.getElementById("title")
+var counter = 0
 
 myButton.addEventListener("click", function () {
     // console.log("Button was clicked!")
-    myButton.style.backgroundColor = 'red'
+    myTitle.textContent = "I'm learning javascript"
+    myButton.textContent = counter
 })
