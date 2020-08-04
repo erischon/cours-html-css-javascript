d9860a9515f7d2a22bb6fe7fa2156952e02ea958
Eri Schon <erischon@gmail.com>
2020-08-04 13:51:08 +0200

    ITJ loops

---
+++ fdcp-3-making_websites_interactive/fdcp-3b-intro-to-javascript/index_10_loops.js
@@ -0,0 +1,35 @@
+// Javascript Loops
+
+
+// for(var i = 0; i < 100; i++){
+//     console.log( i+1 )
+// }
+
+// for(var i = 0; i < 10; i++){
+//     console.log("I'm getting GOOD at for loops")
+// }
+
+
+// for(var i = 0; i < 10; i += 2){
+//     console.log(i)
+// }
+
+// var favFoods = ["pizza", "pasta", "ice cream", "banana"]
+
+// console.log(favFoods[0])
+// console.log(favFoods[1])
+// console.log(favFoods[2])
+// console.log(favFoods[3])
+
+// for(var i = 0; i < favFoods.length; i++) {
+//     console.log( favFoods[i] )
+// }
+
+var numbers = [1, 2, 3, 4, 5, 6]
+// numbers[0]
+
+for(var i = 0; i < numbers.length; i++){
+    if(numbers[i] % 2 === 0){
+        console.log(numbers[i])
+    }
+}
