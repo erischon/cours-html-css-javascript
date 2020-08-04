2bd1e5f47819e07613402b8b6b9c0e37a43a431e
Eri Schon <erischon@gmail.com>
2020-08-04 09:56:54 +0200

    ITJ conditional

--- fdcp-3-making_websites_interactive/fdcp-3b-intro-to-javascript/index_7_conditionals.js
+++ fdcp-3-making_websites_interactive/fdcp-3b-intro-to-javascript/index_7_conditionals.js
@@ -1,10 +1,19 @@
 // Conditionals 
 // Boolean - true / false
 
-if(2 === 4){
-    console.log("TWO IS EQUAL TO FOUR")
-} else if(2 === 5){
-    console.log("TWO IS EQUAL TO FIVE")
-} else if(2 === 2){
-    console.log("TWO IS EQUAL TO TWO")
+// if(2 === 4){
+//     console.log("TWO IS EQUAL TO FOUR")
+// } else if(2 === 5){
+//     console.log("TWO IS EQUAL TO FIVE")
+// } else if(2 === 3){
+//     console.log("TWO IS EQUAL TO THREE")
+// } else {
+//     console.log("I dont know what's going on")
+// }
+
+// Comparison Operator
+   // >, <, <=, >=, ==, ===, !=, !== 
+   
+   if("2" == 2){
+    console.log("They are equal")
 }
