7fa4a14a1d1c437a2c9ebb15ce899d8c4f0ea1ae
Eri Schon <erischon@gmail.com>
2020-08-05 09:29:08 +0200

    ITJ conventions

---
+++ fdcp-3-making_websites_interactive/fdcp-3b-intro-to-javascript/index_18_conventions.js
@@ -0,0 +1,56 @@
+// JS Conventions
+
+// casing - camelCased
+
+var myLocationIsInUtah = true
+
+// white space
+
+for(var i = 0; i < 10; i++){
+    
+}
+
+// naming
+
+var fName = "nate"
+var lName = "jensen"
+
+var friends = []
+var isDone = true
+var isGameOver = false
+
+function sum(num1, num2){
+    
+}
+
+
+// bracket pair colorizor
+// braces
+var person = {
+    address: {
+       street: "123 street" 
+    }
+}
+
+// tab (indenting)
+if(2 === 2){
+    console.log("they are equal")
+    if(3 === 3){
+        console.log("They are equal")    
+    }
+}
+
+
+// HTML tags
+    // <div> 
+
+// comments
+/*
+    anything in between these are commented out
+*/
+
+
+// unix casing
+    // silly cia - no
+    // Silly Cia - no 
+    // silly_cia - yes
