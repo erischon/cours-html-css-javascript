d448b34106f7774918880b3e6bf60398c7c95a06
Eri Schon <erischon@gmail.com>
2020-08-05 14:21:52 +0200

    intro to the dom

---
+++ fdcp-3-making_websites_interactive/fdcp-3c-intro-to-the-dom/index_7.js
@@ -0,0 +1,19 @@
+// Modifying Styles
+
+// var header = document.getElementById("header")
+// document.write(typeof header.style)
+
+// document.write  (Object.keys(header.style))
+
+// header.style.color = "blue"
+
+// fontSize
+// backgroundColor
+// display
+// textAlign
+// margin, padding, border
+
+// var body = document.querySelector("body")
+// body.style.backgroundColor = "#000000"
+
+document.body.style.backgroundColor = "burlywood"
