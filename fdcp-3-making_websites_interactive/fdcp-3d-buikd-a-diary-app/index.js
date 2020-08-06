df4562cb436db28ec893701592d1d898e6afb355
Eri Schon <erischon@gmail.com>
2020-08-06 15:14:46 +0200

    build a diary 9

--- fdcp-3-making_websites_interactive/fdcp-3d-buikd-a-diary-app/index.js
+++ fdcp-3-making_websites_interactive/fdcp-3d-buikd-a-diary-app/index.js
@@ -14,6 +14,10 @@ const entryTextbox = document.getElementsByClassName("entry-textbox")
 
 function addEntryToDom(event) {
     event.preventDefault();
+    const entryDiv = document.createElement('div');
+    entryDiv.className = 'single-entry';
+    entryDiv.innerText = entryTextbox[0].value
+    entriesSection.appendChild(entryDiv);
 }
 
 entryForm.addEventListener('submit', addEntryToDom)
@@ -21,5 +25,15 @@ entryForm.addEventListener('submit', addEntryToDom)
 /*
     create a new variable named "entriesSection" and assign it
     a value of the HTML section we just created
+*/
+
+/*
+  - Inside the addEntryToDom function, create a new variable named entryDiv.
+  - Use the document.createElement method to create an html Div and make it the
+      new variable's value.
+  - On a new line, give the new div a class of 'single-entry'.
+  - Change the div's innerText property to the user's input from the textbox
+  - Use the appendChild method to add the new div to the entriesSection
+      variable created on line 2
 */
 
