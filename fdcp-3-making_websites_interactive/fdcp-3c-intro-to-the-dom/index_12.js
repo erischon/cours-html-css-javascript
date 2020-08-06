4e0ecad3b03d910030e49679b1aad83e24f4ea1c
Eri Schon <erischon@gmail.com>
2020-08-06 11:42:13 +0200

    intro to the dom 12

---
+++ fdcp-3-making_websites_interactive/fdcp-3c-intro-to-the-dom/index_12.js
@@ -0,0 +1,16 @@
+var powerRangers = [
+    "Jason Lee Scott", 
+    "Kimberly Hart", 
+    "Zack Taylor", 
+    "Trini Kwan", 
+    "Billy Cranston"
+]
+
+var rangersList = document.getElementById("rangers")
+
+for(var i = 0; i < powerRangers.length; i++) {
+    var newName = document.createElement("li")
+    newName.textContent = powerRangers[i]
+    rangersList.append(newName)
+}
+
