7734208688697b481380824ab44bd455884b998a
Eri Schon <erischon@gmail.com>
2020-08-06 11:54:29 +0200

    intro to the dom 12

--- fdcp-3-making_websites_interactive/fdcp-3c-intro-to-the-dom/index_12.js
+++ fdcp-3-making_websites_interactive/fdcp-3c-intro-to-the-dom/index_12.js
@@ -6,11 +6,25 @@ var powerRangers = [
     "Billy Cranston"
 ]
 
+// var powerRangers = [
+//     {name: "Jason Lee Scott", color: "Red"}, 
+//     {name: "Kimberly Hart", color: "Pink"}, 
+//     {name: "Zack Taylor", color: "Black"}, 
+//     {name: "Trini Kwan", color: "Yellow"}, 
+//     {name: "Billy Cranston", color: "Blue"},
+//     {name: "Tommy Oliver", color: "Green"}
+// ]
+
+// var rangersList = document.getElementById("rangers")
+
+// for(var i = 0; i < powerRangers.length; i++) {
+//     var newName = document.createElement("li")
+//     newName.textContent = powerRangers[i]
+//     rangersList.append(newName)
+// }
+
 var rangersList = document.getElementById("rangers")
 
-for(var i = 0; i < powerRangers.length; i++) {
-    var newName = document.createElement("li")
-    newName.textContent = powerRangers[i]
-    rangersList.append(newName)
+for (var i = 0; i < powerRangers.length; i++) {
+    rangersList.innerHTML += "<li>" + powerRangers[i] + "</li>"
 }
-
