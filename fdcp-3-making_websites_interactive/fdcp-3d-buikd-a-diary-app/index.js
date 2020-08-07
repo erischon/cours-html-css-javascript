90c457e2a78a973c005c2324eab4ac1f900e163e
Eri Schon <erischon@gmail.com>
2020-08-07 10:38:06 +0200

    build a diary 18

--- fdcp-3-making_websites_interactive/fdcp-3d-buikd-a-diary-app/index.js
+++ fdcp-3-making_websites_interactive/fdcp-3d-buikd-a-diary-app/index.js
@@ -1,74 +1,32 @@
-/*
- - Create a variable named "entryForm" and use the document.getElementById method
-   to make its value the entryForm from our HTML document.
-*/
+const entryForm = document.getElementById('entryForm');
+const entriesSection = document.querySelector('#entries');
+const entryTextbox = document.querySelector('.entry-textbox');
+const entriesNav = document.querySelector('.entries-nav');
 
-const entryForm = document.getElementById("entryForm")
-var entriesSection = document.querySelector("#entries")
-const entryTextbox = document.querySelector(".entry-textbox")
-const entriesNav = document.querySelector('.entries-nav')
-
-/*
- - Add an event listener to the entryForm variable that prevents the page from
-   reloading whenever the submit button is clicked.
-*/
-
-let count = 1
-
+let count = 1;
 function addEntryToDom(event) {
     event.preventDefault();
     const entryDiv = document.createElement('div');
     entryDiv.className = 'single-entry';
-    entryDiv.innerText = entryTextbox.value
+    entryDiv.innerText = entryTextbox.value;
     entryDiv.style.display = 'none';
     entriesSection.appendChild(entryDiv);
+    entryTextbox.value = '';
+    
+    const displayEntryButton = document.createElement('button');
+    displayEntryButton.className = 'display-entry-button';
+    displayEntryButton.innerText = count;
+    entriesNav.appendChild(displayEntryButton);
 
-        /*
-    clear the entry textbox whenever the submit button is clicked
-    */
-
-   entryTextbox.value = '';
-
-   const displayEntryButton = document.createElement('button');
-   displayEntryButton.className = 'display-entry-button';
-   displayEntryButton.innerText = count;
-   entriesNav.appendChild(displayEntryButton);
-
-   displayEntryButton.addEventListener('click', function() {
-    const allEntries = document.querySelectorAll('single-entry');
-    entryDiv.style.display = 'block';
+    displayEntryButton.addEventListener('click', function() {
+        const allEntries = document.querySelectorAll('.single-entry');
+        for (let index = 0; index < allEntries.length; index++) {
+            allEntries[index].style.display = 'none';
+        }
+        entryDiv.style.display = 'block';        
     })
-
-   count ++
+    
+    count++
 }
 
 entryForm.addEventListener('submit', addEntryToDom)
-
-/*
-    create a new variable named "entriesSection" and assign it
-    a value of the HTML section we just created
-*/
-
-/*
-  - Inside the addEntryToDom function, create a new variable named entryDiv.
-  - Use the document.createElement method to create an html Div and make it the
-      new variable's value.
-  - On a new line, give the new div a class of 'single-entry'.
-  - Change the div's innerText property to the user's input from the textbox
-  - Use the appendChild method to add the new div to the entriesSection
-      variable created on line 2
-*/
-
-/*
-    - Inside the displayEntryButton addEventListener function, create a new variable named 'allEntries'.
-    - use the document.queryselectorAll method to select all elements with the class of 'single-entry' and make that the value of the new allEntries variable.
-*/
-
-/*
-    - Use a for loop to iterate through all the elements in the
-        allEntries variable.
-    - Set each element's style.display property to 'none'.
-*/
-
-
-
