288f9417772532eb0c1312f78ad796be4293e65d
Eri Schon <erischon@gmail.com>
2020-08-31 11:22:58 +0200

    javascript challenges

---
+++ fdcp-4-css-and-javascript-fundamentals/fdcp-4c-javascript-challenges/index_7.js
@@ -0,0 +1,6 @@
+/*
+    Task: Add an event listener for 'keyup'
+    1. Capture the event in a 'let' called 'searchQuery' - storing the value
+    2. Make sure the captured value is lowercase
+    3. Log to the console to demonstrate it works
+*/
