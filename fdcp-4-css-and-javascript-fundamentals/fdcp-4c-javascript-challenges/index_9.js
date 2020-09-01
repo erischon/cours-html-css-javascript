9625d7ffb4a651a81c2c752ff7f14d498cd7f4e7
Eri Schon <erischon@gmail.com>
2020-09-01 16:50:46 +0200

    javascript challenges

---
+++ fdcp-4-css-and-javascript-fundamentals/fdcp-4c-javascript-challenges/index_9.js
@@ -0,0 +1,7 @@
+/*
+    Task: Capture the user's search query
+    1. Add an event listener for 'keyup'
+    2. Capture the event in a 'let' called 'searchQuery' - storing the value
+    3. Make sure the captured value is lowercase
+    4. Log to the console to demonstrate it works
+*/
