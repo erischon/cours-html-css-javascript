eeba7db4aaf01dba531717522f642207d996309f
Eri Schon <erischon@gmail.com>
2020-09-01 17:05:01 +0200

    javascript challenges

--- fdcp-4-css-and-javascript-fundamentals/fdcp-4c-javascript-challenges/index_10.js
+++ fdcp-4-css-and-javascript-fundamentals/fdcp-4c-javascript-challenges/index_10.js
@@ -7,4 +7,6 @@
 */
 document.getElementById("searchInput").addEventListener("keyup", function(event) {
     let searchQuery = event.target.value.toLowerCase();
+    let allNamesDOMCollection = document.getElementsByClassName('name');
+    console.log(allNamesDOMCollection[1].textContent);
 });
