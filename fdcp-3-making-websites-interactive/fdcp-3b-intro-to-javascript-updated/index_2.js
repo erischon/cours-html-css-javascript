2bcd5b7355b53dc94fc6263a208a61d50f5ca455
Eri Schon <erischon@gmail.com>
2020-09-07 15:08:01 +0200

    intro to javascript updated - 2

--- fdcp-3-making-websites-interactive/fdcp-3b-intro-to-javascript-updated/index_2.js
+++ fdcp-3-making-websites-interactive/fdcp-3b-intro-to-javascript-updated/index_2.js
@@ -3,9 +3,22 @@
 //////////////////////////
 
 // Strings
+var name = 'joe is awesome'
 
 // Numbers
+var age = 20
 
 // Booleans
+// true or false
+var isOldEnoughToDrive = false
+// console.log()
 
+var userName =''
+var password = ''
+var isItSubscribe = false
 
+// Solution
+
+// var username = "joe123"
+// var password = "password"
+// var isSubscribed = false
