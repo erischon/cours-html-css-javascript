eaba9221def6e7ee6a58e9cd033b6d1a467d6a84
Eri Schon <erischon@gmail.com>
2020-09-15 10:44:47 +0200

    Intro to javascript - updated 7

---
+++ fdcp-3-making-websites-interactive/fdcp-3b-intro-to-javascript-updated/index_7.js
@@ -0,0 +1,44 @@
+//////////////////
+// Conditionals //
+//////////////////
+
+// How we ask our program a question about our data and then run a specific set of code depending on the answer.
+
+// if(true){
+//     console.log("It is true!")
+// }
+
+// if(false){
+//     console.log("It is true!")
+// }
+
+// if(false){
+//     console.log("It is true!")
+// } else {
+//     console.log("It is false!")
+// }
+
+
+// var color = "blue"
+
+// if(color === "red"){
+//     console.log("It is red!")
+// } else if(color === "yellow"){
+//     console.log("It is yellow!")
+// } else {
+//     console.log("The color is not red or yellow")
+// }
+
+
+// exercise
+
+var username = 'rick123'
+var password = 'aL8#klfo'
+
+if(username !== 'rick123'){
+    console.log('username is incorrect')
+} else if(password !== 'aL8#klfo'){
+    console.log("password is incorrect")
+} else {
+    console.log("Access Granted")
+}
