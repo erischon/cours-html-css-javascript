8f39feb7bfb17ad22102c5ddeed2be8e481a5fdf
Eri Schon <erischon@gmail.com>
2020-09-15 09:17:49 +0200

    intro to javascript updated - 3

--- fdcp-3-making-websites-interactive/fdcp-3b-intro-to-javascript-updated/index_3.js
+++ fdcp-3-making-websites-interactive/fdcp-3b-intro-to-javascript-updated/index_3.js
@@ -2,5 +2,20 @@
 // Complex Data Types - Arrays //
 /////////////////////////////////
 
-// Arrays
+// Arrays - List of ordered items
+// 0 indexed
 
+//0.   //1.   //2. 
+var pets = ["dog", "cat", "bird"]
+console.log(pets[0])
+console.log(pets.length)
+
+var mark = "mark"
+var larry = "larry"
+var joe = "joe"
+
+var friendList = [mark, larry, joe]
+
+console.log(friendList[0])
+console.log(friendList[1])
+console.log(friendList[2])
