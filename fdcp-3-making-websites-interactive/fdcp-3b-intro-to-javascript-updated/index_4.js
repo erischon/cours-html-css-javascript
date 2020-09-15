f0fd06704fefec1a6b1101aa0f4c9ee3d9294971
Eri Schon <erischon@gmail.com>
2020-09-15 09:41:23 +0200

    Intro to javascript - updated 4

---
+++ fdcp-3-making-websites-interactive/fdcp-3b-intro-to-javascript-updated/index_4.js
@@ -0,0 +1,35 @@
+/////////////////////////////////////////
+// Complex Data Types - Objects Part 1 //
+/////////////////////////////////////////
+
+// Objects
+
+// describe a peice of data in depth
+
+// key: value pairs
+var dog = {
+    name: "spot",
+    age: 4,
+    isHungry: true,
+    favToys: ["bone", "ball", "squeeky thing"]
+}
+
+// dot notation
+console.log(dog)
+console.log(dog.name)
+console.log(dog.age)
+
+// bracket notation
+console.log(dog["isHungry"])
+
+
+// exercise
+
+var larry = {
+    name: "Larry",
+    age: "20",
+    friends: ["John", "Lisa", "Al"],
+    ispetOwner: true
+}
+
+console.log(people.age)
