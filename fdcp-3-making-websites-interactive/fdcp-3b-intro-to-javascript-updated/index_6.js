e9d69f69a17fef8a5b1c3255405b65fc444f8fd3
Eri Schon <erischon@gmail.com>
2020-09-15 10:32:30 +0200

    Intro to javascript - updated 6

---
+++ fdcp-3-making-websites-interactive/fdcp-3b-intro-to-javascript-updated/index_6.js
@@ -0,0 +1,46 @@
+//////////////////////////
+// Comparison Operators //
+//////////////////////////
+
+// Math operators: + - / *
+
+// Symbols that allow you to compare pieces of data against one another.
+
+// Evaluate
+// = : assignment operator
+
+// ===  
+
+// var name = "rick"
+
+// console.log(name === "rick")
+// console.log(name !== "rick")
+
+// ==
+
+// var number = 2
+
+// console.log(number == "2")
+// console.log(number != "2")
+
+// > < >= <=
+
+// console.log(2 > 0)
+
+
+// exercise
+
+var name = "rick"
+var age = 10
+
+// console.log(name === "dave")
+console.log(name !== "dave")
+
+// console.log(age !== 10)
+console.log(age === 10)
+
+// console.log(10 < age)
+console.log(10 <= age)
+
+// console.log(age >= 11)
+console.log(age <= 11)
