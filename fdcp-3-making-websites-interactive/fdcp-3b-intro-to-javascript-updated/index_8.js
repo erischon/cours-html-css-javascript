2d027dd03693392f643a8230e75159b9498581a0
Eri Schon <erischon@gmail.com>
2020-09-30 11:48:20 +0200

    intro to javascript updated 8

--- fdcp-3-making-websites-interactive/fdcp-3b-intro-to-javascript-updated/index_8.js
+++ fdcp-3-making-websites-interactive/fdcp-3b-intro-to-javascript-updated/index_8.js
@@ -3,3 +3,48 @@
 /////////////////////
 
 // All data in javascript have an inherently Truthy or Falsey value.
+
+// true false
+// "dave"
+
+// if("dave"){
+//     console.log("It is true!")
+// }
+
+// Falsey Values
+ // 0
+ // ""
+ // null
+ // undefined
+ // false
+ // NaN
+
+// if(""){
+//     console.log("It is true!")
+// } else {
+//     console.log("It is false!")
+// }
+
+// if([1, 2, 3, 4]){
+//     console.log("It is true!")
+// } else {
+//     console.log("It is false!")
+// }
+
+// if(null){
+//     console.log("It is true!")
+// } else {
+//     console.log("It is false!")
+// }
+
+var user = {
+    name: "DAVE",
+    email: null,
+    friends: ["rick", "sarah"],
+    address: {
+        street: "123 street",
+        city: "gotham"
+    },
+    id: "0",
+    nickname: undefined
+}
